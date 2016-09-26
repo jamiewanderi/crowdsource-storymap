1b58687b5a9d048d34b4eee858d4dd555570b1c1
zrobby <zach.m.robinson@gmail.com>
2016-09-26 15:33:21 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -45,18 +45,23 @@ function main() {
     var coords3;
     var coords4;
     
+    var streets = L.tileLayer('https://api.mapbox.com/styles/v1/zachrobinsonalta/citahgib4000o2iqbp4k46wg1/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiemFjaHJvYmluc29uYWx0YSIsImEiOiJjaXQyMjNucXQwc3NvMnBraHF5cnQ3M3g0In0.zXNUiATCOwYfTikOPJ311Q', {attribution: '© Mapbox © OpenStreetMap'}),
+        satellite = L.tileLayer('https://api.mapbox.com/styles/v1/zachrobinsonalta/citagtu5u000k2iqbtmpc09f3/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiemFjaHJvYmluc29uYWx0YSIsImEiOiJjaXQyMjNucXQwc3NvMnBraHF5cnQ3M3g0In0.zXNUiATCOwYfTikOPJ311Q', {attribution: '© Mapbox © OpenStreetMap © DigitalGlobe'});
     
     var map = L.map('map', {
         center: [35.1108, -120.5917],
         zoom: 14,
+        layers: [streets]
     });
     
-    L.tileLayer('https://api.mapbox.com/styles/v1/zachrobinsonalta/citahgib4000o2iqbp4k46wg1/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiemFjaHJvYmluc29uYWx0YSIsImEiOiJjaXQyMjNucXQwc3NvMnBraHF5cnQ3M3g0In0.zXNUiATCOwYfTikOPJ311Q', {attribution: '© Mapbox © OpenStreetMap'}).addTo(map);
-    
+    var basemaps = {
+        "Streets": streets,
+        "Satellite": satellite
+    }
+
     cartodb.createLayer(map, {
           user_name: 'zacharyrobinson',
           type: 'cartodb',
-          //refreshTime: 2000,
           sublayers: [{
             sql: "SELECT * FROM point_collection",
             cartocss: '#point_collection{marker-fill-opacity: 0.9; marker-line-color: #FFF;  marker-line-width: 1;  marker-line-opacity: 1;  marker-placement: point;  marker-type: ellipse;  marker-width: 15;  marker-fill: #0066FF;  marker-allow-overlap: true;}'
@@ -65,6 +70,10 @@ function main() {
         .addTo(map)
         .done(function(layer) {
             my_layer = layer;
+            var overlay = {
+                "Public Comments": my_layer
+            };
+            L.control.layers(basemaps, overlay, {collapsed: false}).addTo(map);
             cartodb.vis.Vis.addInfowindow(map, layer, ['description'])
         });
     
